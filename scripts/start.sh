7fcc305fff460654bd791af7c5f4e652c2615510
Keisuke Ishigami <keisuke.130@icloud.com>
2023-11-15 13:09:33 +0900

    feature: sepolia にデプロイするようにしました

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## バックグラウンドで開発用ローカルブロックチェーンが起動されるのを待つ
-sleep 5 && npx hardhat --max-memory 518 compile && npm run deploylocal &
+sleep 5 && npx hardhat --max-memory 518 compile && npm run deploysepolia &
 
 # 開発用ローカルブロックチェーンの起動
 npm run node
