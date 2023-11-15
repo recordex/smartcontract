2e9c96f5e87715dac6061983ddeff0d9896efb9b
Keisuke Ishigami <keisuke.130@icloud.com>
2023-11-15 09:57:52 +0900

    test

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## バックグラウンドで開発用ローカルブロックチェーンが起動されるのを待つ
-#sleep 5 && npx hardhat --max-memory 518 compile && npm run deploylocal
+sleep 5 && npx hardhat --max-memory 518 compile && npm run deploylocal &
 
 # 開発用ローカルブロックチェーンの起動
-npm run node --hostname 0.0.0.0
+npm run node
